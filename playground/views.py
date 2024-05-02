1ddb401fb012532fe795c55d3de50250f4972f7a
Raghav Nair <nairraghav14@gmail.com>
2024-05-02 14:45:51 +0530

    Django Orm : Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q, F
 from store.models import Product, Customer, Collection, Order, OrderItem
 
 # Create your views here.
 
 def say_hello(request):
 
-    # inventory < 10 AND price < 20
-    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # inventory < 10 OR price < 20
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     return render(request,'hello.html', {'products': list(queryset)})
