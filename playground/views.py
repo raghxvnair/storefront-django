03a82d47143de17499e2e94e4abb4e118b67021e
Raghav Nair <nairraghav14@gmail.com>
2024-05-16 21:48:28 +0530

    Custom Manager

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.db.models import Q, F
-from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Customer, Collection, Order, OrderItem
-
+from tags.models import TaggedItem
 # Create your views here.
 
 def say_hello(request):
-    # queryset = Order.objects.aggregate(count=Count('id'))
-    # queryset = OrderItem.objects.filter(product__id=1).aggregate(sum=Sum('quantity'))
-    # queryset = Order.objects.filter(customer__id=1).aggregate(count=Count('id'))
-    queryset = Product.objects.filter(collection__id=3).aggregate(min=Min('unit_price'), max=Max('unit_price'), avg=Avg('unit_price'))
 
+    queryset = TaggedItem.objects.get_tags_for(Product,1)
 
-    return render(request,'hello.html', {'orders': queryset})
+    return render(request,'hello.html', {'orders': list(queryset)})
