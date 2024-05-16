f3bd80be3d6350f2c144d7fc4227c90a52c38dce
Raghav Nair <nairraghav14@gmail.com>
2024-05-16 21:17:29 +0530

    Filter-Aggregate

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Customer, Collection, Order, OrderItem
 
 # Create your views here.
 
 def say_hello(request):
+    # queryset = Order.objects.aggregate(count=Count('id'))
+    # queryset = OrderItem.objects.filter(product__id=1).aggregate(sum=Sum('quantity'))
+    # queryset = Order.objects.filter(customer__id=1).aggregate(count=Count('id'))
+    queryset = Product.objects.filter(collection__id=3).aggregate(min=Min('unit_price'), max=Max('unit_price'), avg=Avg('unit_price'))
 
-    # inventory = price 
-    products = OrderItem.objects.values('product_id').distinct()
-    queryset = Product.objects.filter(id__in=products).order_by('title')
-    return render(request,'hello.html', {'products': list(queryset)})
+
+    return render(request,'hello.html', {'orders': queryset})
